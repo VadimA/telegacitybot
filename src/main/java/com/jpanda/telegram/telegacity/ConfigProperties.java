d250cc435f71420e4f7560579aa2ea166c3c1005
VadimA <wadimanosov@gmail.com>
2017-08-17 02:47:29 +0400

    Changed loading of property file.

--- src/main/java/com/jpanda/telegram/telegacity/ConfigProperties.java
+++ src/main/java/com/jpanda/telegram/telegacity/ConfigProperties.java
@@ -12,31 +12,24 @@ import java.util.Properties;
  * @author Alexey Storozhev
  */
 public class ConfigProperties {
-    private static Map<String, String> props = new HashMap<String, String>();
+
     private static String CONFIG_FILE = "config.properties";
+
+    static Properties property = new Properties();
 
     public static void init() {
         FileInputStream fis;
-        Properties property = new Properties();
-
         try {
             fis = new FileInputStream("src/main/resources/" + CONFIG_FILE);
             property.load(fis);
-            Enumeration<?> e = property.propertyNames();
-            while (e.hasMoreElements()) {
-                String key = (String) e.nextElement();
-                String value = new String(property.getProperty(key).getBytes("ISO-8859-1"), "UTF-8");
-                props.put(key, value);
-            }
-
         } catch (IOException e) {
-            System.err.println("ОШИБКА: Файл свойств отсуствует!");
+            System.err.println("Exception! Property file is not found.");
         }
 
     }
 
     public static String getProperty(String name) {
-        return props.get(name);
+        return property.getProperty(name);
     }
 
     public static void setConfigFile(String configFile) {
