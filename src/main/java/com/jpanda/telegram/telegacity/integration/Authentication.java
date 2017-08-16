c13fe7ef0a2120b994d8b1fdbf5520e1ac027529
VadimA <wadimanosov@gmail.com>
2017-08-17 02:59:36 +0400

    Added new package for integration with TeamCity

---
+++ src/main/java/com/jpanda/telegram/telegacity/integration/Authentication.java
@@ -0,0 +1,21 @@
+package com.jpanda.telegram.telegacity.integration;
+
+/**
+ * Created by Vadim Anosov on 17.08.2017.
+ */
+public class Authentication {
+
+    private String username;
+
+    private String password;
+
+    public Authentication() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
